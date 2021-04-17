4b792267cdc0aaf14209384ffe59b82823bae574
DIVYAM_MISHRA <divyamroopam19994u@gmail.com>
2021-04-17 23:36:18 +0530

    added signup page

--- app/src/main/java/com/example/homemart/RegisterActivity.java
+++ app/src/main/java/com/example/homemart/RegisterActivity.java
@@ -6,24 +6,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
-    Spinner spinner;
+public class RegisterActivity extends AppCompatActivity {
+
+    private Button CreateAccountButton;
+    private EditText InputUsername, InputPassword, InputRetypePassword, InputPhone;
+
+    Spinner spinner2;
     ArrayAdapter<CharSequence> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
-        spinner = (Spinner) findViewById(R.id.spinner);
+        CreateAccountButton = (Button) findViewById(R.id.register_signup_btn);
+        InputUsername = (EditText) findViewById(R.id.register_username_input);
+        InputPassword = (EditText) findViewById(R.id.register_password_input);
+        InputRetypePassword = (EditText) findViewById(R.id.register_retype_password_input);
+        InputPhone = (EditText) findViewById(R.id.register_phone_input);
+
+
+
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
         adapter = ArrayAdapter.createFromResource(this, R.array.users, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        spinner2.setAdapter(adapter);
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
